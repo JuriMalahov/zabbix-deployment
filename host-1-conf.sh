cbe16eabca1abd11d5da8009ea0a5bab4e8a9bd1
JuriMalahov <144311634+JuriMalahov@users.noreply.github.com>
2025-12-05 21:10:36 +0300

    Add files via upload

---
+++ host-1-conf.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+echo "Настройка сети для первого сервера"
+
+echo "Настройка имени узла"
+hostnamectl set-hostname mon-host-1; /bin/bash
+
+echo "Настройка /etc/hosts"
+sudo cat > /etc/hosts <<EOF
+127.0.0.1       localhost
+127.0.1.1       debian
+
+# The following lines are desirable for IPv6 capable hosts
+::1     localhost ip6-localhost ip6-loopback
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+192.168.1.1     mon-server
+192.168.1.2     mon-host-1
+192.168.1.3     mon-host-2
+192.168.1.4     vesr
+192.168.1.5     mon-server-2
+192.168.1.6     mon-proxy
+EOF
+
+echo "Настройка /etc/network/interfaces"
+sudo cat > /etc/network/interfaces <<EOF
+# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+
+source /etc/network/interfaces.d/*
+
+# The loopback network interface
+auto lo
+iface lo inet loopback
+
+auto enp0s8
+iface enp0s8 inet static
+address 192.168.1.2/24
+EOF
+
+echo "Перезапуск сервиса networking"
+sudo systemctl restart networking
+
+echo "Настройка SSH"
+sudo apt-get update
+sudo apt-get install openssh-server -y
+sudo sed -i 's/^#\?Port.*/Port 22/' /etc/sysctl.conf
+sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin no/' /etc/sysctl.conf
+sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication no/' /etc/sysctl.conf
+
+echo "Перезапуск sshd"
+sudo systemctl restart sshd
+sudo systemctl enable sshd
+
+echo "Копирование ключа"
+sudo mkdir ~/.ssh
+sudo cp authorized_keys ~/.ssh/
---
+++ server-1-conf.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+echo "Настройка сети для первого сервера"
+
+echo "Настройка имени узла"
+hostnamectl set-hostname mon-server; /bin/bash
+
+echo "Настройка /etc/hosts"
+sudo cat > /etc/hosts <<EOF
+127.0.0.1       localhost
+127.0.1.1       debian
+
+# The following lines are desirable for IPv6 capable hosts
+::1     localhost ip6-localhost ip6-loopback
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+192.168.1.1     mon-server
+192.168.1.2     mon-host-1
+192.168.1.3     mon-host-2
+192.168.1.4     vesr
+192.168.1.5     mon-server-2
+192.168.1.6     mon-proxy
+EOF
+
+#echo "Настройка /etc/resolv.conf"
+#sudo cat > /etc/hosts <<EOF
+#nameserver 8.8.8.8
+#EOF
+
+echo "Настройка /etc/network/interfaces"
+sudo cat > /etc/network/interfaces <<EOF
+# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+
+source /etc/network/interfaces.d/*
+
+# The loopback network interface
+auto lo
+iface lo inet loopback
+
+auto enp0s3
+iface enp0s3 inet dhcp
+auto enp0s8
+iface enp0s8 inet static
+address 192.168.1.1/24
+EOF
+
+echo "Перезапуск сервиса networking"
+sudo systemctl restart networking
+
+echo "Включение IP forwarding в /etc/sysctl.conf"
+sudo sed -i 's/^#\?net.ipv4.ip_forward.*/net.ipv4.ip_forward=1/' /etc/sysctl.conf
+
+echo "Применение настроек sysctl"
+sudo sysctl -p
+
+echo "Настройка маскарадинга"
+sudo apt-get update
+sudo apt-get install iptables -y
+sudo iptables -t nat -A POSTROUTING -s 192.168.1.0/24 -o enp0s3 -j MASQUERADE
+
+echo "Сохранение правил iptables в /root/rules"
+sudo iptables-save > /root/rules
+
+echo "Добавление команды восстановления правил iptables при перезапуске в crontab"
+sudo (crontab -l 2>/dev/null; echo "@reboot /sbin/iptables-restore < /root/rules") | sudo crontab -
+
+echo "Настройка SSH"
+sudo apt-get install openssh-server -y
+sudo sed -i 's/^#\?Port.*/Port 22/' /etc/sysctl.conf
+sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin no/' /etc/sysctl.conf
+sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication no/' /etc/sysctl.conf
+
+echo "Перезапуск sshd"
+sudo systemctl restart sshd
+sudo systemctl enable sshd
+
+echo "Копирование ключей и списка хостов"
+sudo mkdir ~/.ssh
+sudo cp id_rsa ~/.ssh/
+sudo cp id_rsa.pub ~/.ssh/
+sudo cp known_hosts ~/.ssh/
